a46854ca9b576fb39d2e954176445975aa6b4ea2
smit479 <smitpanchal464@gmail.com>
2026-06-14 19:39:21 +0530

    Added Tailwind CSS  and Add Others

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { useState } from "react";
-import Editor from "react-simple-code-editor";
-import { highlight, languages } from "prismjs/components/prism-core";
-import "prismjs/components/prism-clike";
-import "prismjs/components/prism-javascript";
-import "prismjs/themes/prism.css";
-import State from './state.js'; 
-import Forms from './form.js'; 
-import Todo from './todo/todo.js'; 
+// import React, { useState } from "react";
+// import Editor from "react-simple-code-editor";
+// import { highlight, languages } from "prismjs/components/prism-core";
+// import "prismjs/components/prism-clike";
+// import "prismjs/components/prism-javascript";
+// import "prismjs/themes/prism.css";
+// import State from './state.js'; 
+// import Forms from './form.js'; 
+// import Todo from './todo/todo.js'; 
+
+// export default function App() {
+//   const [code, setCode] = useState(`function Test () { return "hello"}`);
+  
+//   return (
+//     <>
+//     <h1>React </h1>
+//     </>
+//   );
+// }
 
 export default function App() {
-  const [code, setCode] = useState(`function Test () { return "hello"}`);
-  
   return (
-    <>
-      <State />
-      <Forms />
-      <Todo/>
-    </>
+    <div className="bg-blue-500 text-white p-10 text-center text-3xl">
+      Tailwind Working 🚀
+    </div>
   );
 }
